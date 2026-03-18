01e598caa8708e3e8413829ab908bd81f09ff80a
krishnamohanty9nov-sys <krishnamohanty9nov@gmail.com>
2026-03-18 11:13:53 +0530

    Coded UC5: Display Hello with multiple args using enhanced for loop or default

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,18 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String output;
+        StringBuilder names = new StringBuilder();
 
-        if (args.length > 0) {
-            output = String.join(", ", args);
+        if (args.length == 0) {
+            names.append("World");
         } else {
-            output = "World";
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
         }
 
-        System.out.println("Hello, " + output + "!");
+        System.out.println("Hello, " + names.toString() + "!");
     }
 }
